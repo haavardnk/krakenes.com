6206c3be6856011dc1bc21a2f0c55bcecaceb155
Håvard <haavardnk@outlook.com>
2019-02-09 20:02:16 +0100

    Added author picture to blog

--- krakenes/urls.py
+++ krakenes/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('accounts/', include('accounts.urls')),
     path('blog/', include('blog.urls')),
-    path('projects/', include('projects.urls'))
+    path('projects/', include('projects.urls')),
+    path('avatar/', include('avatar.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
