8c14138a9a39376c74653ee3c2faf3225d6be894
Håvard <haavardnk@outlook.com>
2019-02-09 23:54:15 +0100

    Favicon settings

--- krakenes/settings.py
+++ krakenes/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'krakenes/static/'),
+
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
