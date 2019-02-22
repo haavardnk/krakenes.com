8537cb8346b91cdaf62b3c9eefc6061dd5b45037
haavardnk <haavardnk@outlook.com>
2019-02-22 08:44:37 +0100

    Added travis-ci db in settings.py

--- krakenes/settings.py
+++ krakenes/settings.py
@@ -159,3 +159,19 @@ try:
     from .local_settings import *
 except ImportError:
     pass
+
+import os
+
+# (...)
+
+if 'TRAVIS' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE':   'django.db.backends.postgresql_psycopg2',
+            'NAME':     'travisci',
+            'USER':     'postgres',
+            'PASSWORD': '',
+            'HOST':     'localhost',
+            'PORT':     '',
+        }
+    }
