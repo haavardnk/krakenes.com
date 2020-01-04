9115a9a9cd017a1d22fbb18f8f05ac6023ad7554
Håvard Kråkenes <220888@usn.no>
2020-01-04 20:51:16 +0100

    Blog page basically complete

--- blog/views.py
+++ blog/views.py
@@ -18,14 +18,24 @@ def blog(request):
     all_posts = BlogPost.objects.all().order_by('-id')
 
     if request.method == 'GET' and 'cat' in request.GET:
+        search_vector = SearchVector('category__name')
+        post_list = BlogPost.objects.all().annotate(cat=search_vector).filter(
+            cat=request.GET['cat']).order_by('-id').distinct('id')
+        selected_category = request.GET['cat']
+        search_message = None
+
+    elif request.method == 'GET' and 'search' in request.GET:
         search_vector = SearchVector(
             'author__username', 'category__name', 'title', 'tags__name', 'content')
-        post_list = BlogPost.objects.all().annotate(cat=search_vector).filter(
-            cat=request.GET['cat']).order_by('-id')
+        post_list = BlogPost.objects.all().annotate(search=search_vector).filter(
+            search=request.GET['search']).order_by('-id', 'title').distinct('id')
+        search_message = request.GET['search']
     else:
         post_list = all_posts
+        selected_category = None
+        search_message = None
 
-    paginator = Paginator(post_list, 4)
+    paginator = Paginator(post_list, 6)
     tags = Tag.objects.all()
     categories = Category.objects.all().annotate(posts_count=Count('blogpost'))
 
@@ -33,28 +43,19 @@ def blog(request):
     page = request.GET.get('page')
     posts = paginator.get_page(page)
 
-    # Search for blog post
-    if request.method == 'GET' and 'cat' in request.GET:
-        # If result:
-        if post_list:
-            return render(request, 'blog/blog.html', {
-                'posts': posts,
-                'all_posts': all_posts,
-                'range': range(posts.paginator.num_pages+1),
-                'tags': tags,
-                'categories': categories,
-                'selected_category': request.GET['cat']
-            })
-        # If no result:
+    # If selected category and/or search
+    if request.method == 'GET' and ('cat' or 'search') in request.GET:
         return render(request, 'blog/blog.html', {
             'posts': posts,
             'all_posts': all_posts,
             'range': range(posts.paginator.num_pages+1),
             'tags': tags,
             'categories': categories,
-            'selected_category': 'There are no results that match your search.'
+            'selected_category': selected_category,
+            'search_message': search_message
         })
-    # No search:
+
+    # No search or selected category:
     return render(request, 'blog/blog.html', {
         'posts': posts,
         'all_posts': all_posts,
