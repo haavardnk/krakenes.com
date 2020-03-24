801254322d4121ba8d819b1d6b0a405e8bba3e2b
Håvard Kråkenes <haavardnk@outlook.com>
2020-03-24 12:49:34 +0100

    Disabled sign up tests from old system

--- accounts/tests.py
+++ accounts/tests.py
@@ -51,50 +51,50 @@ class LoginTests(BaseTestCase):
         self.assertEqual(json.loads(response.content)['login'], "failed")
 
 
-class SignupTests(BaseTestCase):
-    '''
-    Tests of signup
-    '''
+# class SignupTests(BaseTestCase):
+#     '''
+#     Tests of signup
+#     '''
 
-    def test_signup_success(self):
-        '''
-        Tests a successful signup
-        '''
-        response = self.client.post("/accounts/signup", {"username": "testuser2", "password1": "testpass",
-                                                         "password2": "testpass", "email": "test@email.com", "next": "/"}, **{'HTTP_X_REQUESTED_WITH': 'XMLHttpRequest'})
-        self.assertEqual(json.loads(response.content)['register'], "success")
+#     def test_signup_success(self):
+#         '''
+#         Tests a successful signup
+#         '''
+#         response = self.client.post("/accounts/signup", {"username": "testuser2", "password1": "testpass",
+#                                                          "password2": "testpass", "email": "test@email.com", "next": "/"}, **{'HTTP_X_REQUESTED_WITH': 'XMLHttpRequest'})
+#         self.assertEqual(json.loads(response.content)['register'], "success")
 
-    def test_signup_username_taken(self):
-        '''
-        Tests username taken
-        '''
-        response = self.client.post("/accounts/signup", {"username": "testuser", "password1": "testpass",
-                                                         "password2": "testpass", "email": "test@email.com", "next": "/"}, **{'HTTP_X_REQUESTED_WITH': 'XMLHttpRequest'})
-        self.assertEqual(json.loads(response.content)['register'], "user")
+#     def test_signup_username_taken(self):
+#         '''
+#         Tests username taken
+#         '''
+#         response = self.client.post("/accounts/signup", {"username": "testuser", "password1": "testpass",
+#                                                          "password2": "testpass", "email": "test@email.com", "next": "/"}, **{'HTTP_X_REQUESTED_WITH': 'XMLHttpRequest'})
+#         self.assertEqual(json.loads(response.content)['register'], "user")
 
-    def test_signup_email_taken(self):
-        '''
-        Tests email taken
-        '''
-        response = self.client.post("/accounts/signup", {"username": "testuser3", "password1": "testpass",
-                                                         "password2": "testpass", "email": "test@test.com", "next": "/"}, **{'HTTP_X_REQUESTED_WITH': 'XMLHttpRequest'})
-        self.assertEqual(json.loads(response.content)['register'], "email")
+#     def test_signup_email_taken(self):
+#         '''
+#         Tests email taken
+#         '''
+#         response = self.client.post("/accounts/signup", {"username": "testuser3", "password1": "testpass",
+#                                                          "password2": "testpass", "email": "test@test.com", "next": "/"}, **{'HTTP_X_REQUESTED_WITH': 'XMLHttpRequest'})
+#         self.assertEqual(json.loads(response.content)['register'], "email")
 
-    def test_signup_passwords_not_equal(self):
-        '''
-        Tests passwords not equal
-        '''
-        response = self.client.post("/accounts/signup", {"username": "testuser3", "password1": "testpass",
-                                                         "password2": "passtest", "email": "test@pass.com", "next": "/"}, **{'HTTP_X_REQUESTED_WITH': 'XMLHttpRequest'})
-        self.assertEqual(json.loads(response.content)['register'], "password")
+#     def test_signup_passwords_not_equal(self):
+#         '''
+#         Tests passwords not equal
+#         '''
+#         response = self.client.post("/accounts/signup", {"username": "testuser3", "password1": "testpass",
+#                                                          "password2": "passtest", "email": "test@pass.com", "next": "/"}, **{'HTTP_X_REQUESTED_WITH': 'XMLHttpRequest'})
+#         self.assertEqual(json.loads(response.content)['register'], "password")
 
-    def test_signup_not_post(self):
-        '''
-        Tests a get request to the signup view
-        '''
-        response = self.client.get("/accounts/signup", {"username": "testuser3", "password1": "testpass",
-                                                        "password2": "passtest", "email": "test@pass.com", "next": "/"}, **{'HTTP_X_REQUESTED_WITH': 'XMLHttpRequest'})
-        self.assertEqual(json.loads(response.content)['register'], "failed")
+#     def test_signup_not_post(self):
+#         '''
+#         Tests a get request to the signup view
+#         '''
+#         response = self.client.get("/accounts/signup", {"username": "testuser3", "password1": "testpass",
+#                                                         "password2": "passtest", "email": "test@pass.com", "next": "/"}, **{'HTTP_X_REQUESTED_WITH': 'XMLHttpRequest'})
+#         self.assertEqual(json.loads(response.content)['register'], "failed")
 
 
 class LogoutTests(BaseTestCase):
