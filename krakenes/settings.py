efc847fe8b2eefa1f73409e0443e5475986a625f
Håvard Kråkenes <haavardnk@outlook.com>
2020-03-24 19:19:23 +0100

    Removed url, added forgotten password in modal

--- krakenes/settings.py
+++ krakenes/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'dummykey')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DJANGO_DEBUG', False)
+DEBUG = os.getenv('DJANGO_DEBUG', True)
 
 ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '127.0.0.1').split(',')
 
